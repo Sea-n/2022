aba79074d3077ddea3a537a787d867d9763d6866
gnehs <gnehs@users.noreply.github.com>
2022-04-27 15:23:49 +0000

    deploy: 0f15a1f67a04790ccff5fba11a7cbedb714f1aa2

---
+++ _nuxt/index-48f40a7a.mjs
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c as t}from"./entry-b29879ea.mjs";const c={created(){this.$router.push("/cfp")}};function o(s,a,n,p,_,d){return r(),t("div")}var i=e(c,[["render",o]]);export{i as default};
